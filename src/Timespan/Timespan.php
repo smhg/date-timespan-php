60dfb8fd145b442eb633958956364a03b4510b13
smhg <sam@strictlyphp.com>
2014-03-08 14:23:13 +0100

    timespan tests

--- src/Timespan/Timespan.php
+++ src/Timespan/Timespan.php
@@ -1,6 +1,141 @@
 <?php
 namespace Timespan;
 
+use \DateTime;
+use \DateInterval;
+use \DatePeriod;
+
 class Timespan
 {
+    public $start;
+    public $end;
+
+    public function __construct(DateTime $start, DateTime $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    /**
+     * Check whether the timespan contains a date
+     * @param DateTime $date
+     * @return boolean
+     */
+    public function contains(DateTime $date)
+    {
+        return $this->start < $date && $date < $this->end;
+    }
+
+    /**
+     * Check whether the timespan overlaps with another timespan
+     * @param Timespan $span
+     * @return boolean
+     */
+    public function overlaps(Timespan $span)
+    {
+        return $this->start < $span->end && $span->start < $this->end;
+    }
+
+    public function canBeCompressedWith(Timespan $span)
+    {
+        return ($this->overlaps($span) || $this->end == $span->start || $this->start == $span->end);
+    }
+
+    /**
+     * Get parts of the timespan which don't appear in another timespan
+     * @param Timespan $span
+     * @return Collection
+     */
+    public function diff(Timespan $span)
+    {
+        $collection = new Collection();
+
+        if (!$this->overlaps($span)) {
+            $collection[] = clone $this;
+        } else {
+            if ($span->start <= $this->start) {
+                // start before/together
+                if ($this->end > $span->end) {
+                    // eindigt erin
+                    $collection[] = new $this($span->end, $this->end);
+                }
+                // end after/together
+            } else {
+                // start inside
+                if ($span->end < $this->end) {
+                    // end inside
+                    $collection[] = new $this($this->start, $span->start);
+                    $collection[] = new $this($span->end, $this->end);
+                } else {
+                    // end after/together
+                    $collection[] = new $this($this->start, $span->start);
+                }
+            }
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Merge the timespan with another one (if possible)
+     * @param Timespan $span
+     * @return Timespan
+     */
+    public function merge(Timespan $span)
+    {
+        if ($this->start > $span->start) {
+            $this->start = $span->start;
+        }
+
+        if ($this->end < $span->end) {
+            $this->end = $span->end;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Compare the timespan with another timespan
+     * @param Timespan $span
+     * @return boolean
+     */
+    public function compare(Timespan $span)
+    {
+        if ($this->start == $span->start) {
+            return 0;
+        }
+        return $this->start < $span->start ? -1 : 1;
+    }
+
+    /**
+     * Convert the timespan into a period based on an interval
+     * @param DateInterval $interval
+     * @return DatePeriod
+     */
+    public function toPeriod(DateInterval $interval)
+    {
+        $end = clone ($this->end);
+        return new DatePeriod($this->start, $interval, $end->modify('+1 second'));
+    }
+
+    /**
+     * Convert the timespan to an array
+     * @return array
+     */
+    public function toArray()
+    {
+        return array(
+            'start' => $this->start->format('c'),
+            'end' => $this->end->format('c')
+        );
+    }
+
+    /**
+     * Convert the timespan to an array
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->start->format('c') . ' - ' . $this->end->format('c');
+    }
 }
--- tests/TimespanTest.php
+++ tests/TimespanTest.php
@@ -13,5 +13,107 @@ class TimespanTest extends PHPUnit_Framework_TestCase
         $end->modify('+1 week');
 
         $span = new Timespan($start, $end);
+
+        $this->assertEquals($start, $span->start);
+        $this->assertEquals($end, $span->end);
+
+        return $span;
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testContains($span)
+    {
+        $date = clone $span->start;
+        $date->modify('+3 day');
+
+        $this->assertTrue($span->contains($date));
+        $this->assertFalse($span->contains($date->modify('-1 week')));
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testToPeriod($span)
+    {
+        $period = $span->toPeriod(new \DateInterval('P1D'));
+        $this->assertInstanceOf('DatePeriod', $period);
+        $arr = iterator_to_array($period);
+        $this->assertEquals($span->start, reset($arr));
+        $this->assertEquals($span->end, end($arr));
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testToArray($span)
+    {
+        $arr = $span->toArray();
+        $this->assertTrue(isset($arr['start']));
+        $this->assertTrue(isset($arr['end']));
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testToString($span)
+    {
+        $this->assertTrue(is_string((string)$span));
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testOverlaps($span)
+    {
+        $start = clone $span->start;
+        $start->modify('+3 day');
+        $end = clone $start;
+        $end->modify('+1 week');
+
+        $new = new Timespan($start, $end);
+        $this->assertTrue($new->overlaps($span));
+        $this->assertTrue($span->overlaps($new));
+
+        $start = clone $span->start;
+        $end = clone $span->end;
+
+        $new = new Timespan($start, $end);
+        $this->assertTrue($new->overlaps($span));
+        $this->assertTrue($span->overlaps($new));
+
+        $start = clone $span->start;
+        $start->modify('+2 week');
+        $end = clone $start;
+        $end->modify('+1 week');
+
+        $new = new Timespan($start, $end);
+        $this->assertFalse($new->overlaps($span));
+        $this->assertFalse($span->overlaps($new));
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testDiff($span)
+    {
+
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testMerge($span)
+    {
+
+    }
+
+    /**
+     * @depends testConstructor
+     */
+    public function testCompare($span)
+    {
+
     }
 }
